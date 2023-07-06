d665a0032179ccdd34fdc6fa9ffdee883f0b1719
Ben Kaye <ben.kaye@organox.com>
2023-07-06 12:33:55 +0100

    refactor for readability

--- plibs/backend.py
+++ plibs/backend.py
@@ -7,21 +7,15 @@ import scipy.signal as sps
 import scipy as sp
 import numpy as np
 
-
-from dash.exceptions import PreventUpdate
-
 import math
-import io
-import csv
 
-
-def make_spectrum(hx, hy, ch, ranges, fns):
+def make_spectrum(col_x, col_y, df, ranges, fns):
     trs = []
     for fn in fns:
-        cache = ch.get(fn)
+        cache = df.get(fn)
         rng = ranges[fn] if type(
             ranges) is dict and ranges.get(fn) else [0, None]
-        trs.append(make_spectrum_tr(hx, hy, cache, rng))
+        trs.append(make_spectrum_tr(col_x, col_y, cache, rng))
 
     fig = psp.make_subplots()
     fig.add_traces(trs)
@@ -29,16 +23,14 @@ def make_spectrum(hx, hy, ch, ranges, fns):
     return fig
 
 
-def make_spectrum_tr(hx, hy, ch, rng):
-    t = ch.get(hx)[rng[0]:rng[1]]
+def make_spectrum_tr(col_x, col_y, df, rng):
+    t = df.get(col_x)[rng[0]:rng[1]]
     fs = 1/np.mean(np.diff(t))
 
-    y = ch.get(hy)[rng[0]:rng[1]]
+    y = df.get(col_y)[rng[0]:rng[1]]
     f, Pxx = sps.periodogram(x=y, fs=fs)
 
-    # fig = psp.make_subplots()
     tr = go.Scatter(x=f, y=Pxx, mode='lines')
-    # fig.add_trace(tr)
 
     return tr
 
@@ -53,92 +45,51 @@ def get_col_val(col_idxs, r):
         yield val
 
 
-def trace(x, y):
-    return go.Scatter(x=x, y=y)
-
-
 def multiplot(*, fig,  x, y, y2=None):
+    '''
+    Adds data to an existing figure
+
+    fig: plotly.Figure
+    x: x data as iterable
+    y: list of y data
+    y2: <optional> list of y data for 2nd axis
+    '''
     if y2 is None:
-        # fig = go.Figure()
         left_arg = dict()
     else:
-        # fig = psp.make_subplots(specs=[[{"secondary_y": True}]])
         left_arg = dict(secondary_y=False)
         right_arg = dict(secondary_y=True)
 
-    if len(y) > 0 and hasattr(y[0], '__len__'):
-        for y_k in y:
-            fig.add_trace(trace(x, y_k), **left_arg)
-    else:
-        fig.add_trace(trace(x, y), **left_arg)
+    def trace(x, y):
+        return go.Scatter(x=x, y=y)
 
-    if y2 is not None:
-        if len(y2) > 0 and hasattr(y2[0], '__len__'):
-            for y2_k in y2:
-                fig.add_trace(trace(x, y2_k), **right_arg)
+    def add_traces(x, y_list, args):
+        if len(y_list) > 0 and hasattr(y_list[0], '__len__'):
+            for y_k in y:
+                fig.add_trace(trace(x, y_k), **args)
         else:
-            fig.add_trace(trace(x, y2), **right_arg)
+            fig.add_trace(trace(x, y), **args)
+
+    add_traces(x, y, left_arg)
+    if y2 is not None:
+        add_traces(x, y2, right_arg)
 
 
-def make_fig(h_x, h_ys, h_ays, data, id_range, filt, fns):
+def make_fig(col_x, cols_y, cols_y2, data, id_range, filt, fns):
 
-    secondary = bool(len(h_ays) > 0)
+    secondary = bool(len(cols_y2) > 0)
     fig = go.Figure() if not secondary else psp.make_subplots(
         specs=[[{"secondary_y": True}]])
 
     for fn in fns:
         dct = data[fn]
-        x = dct[h_x]
-        y = [dct[h_y] for h_y in h_ys]
-        y2 = [dct[h_y2] for h_y2 in h_ays] if secondary else None
+        x = dct[col_x]
+        y = [dct[col] for col in cols_y]
+        y2 = [dct[col] for col in cols_y2] if secondary else None
 
         multiplot(fig=fig, x=x, y=y, y2=y2)
 
     return fig
-
-
-def make_fig_traces(h_x, h_ys, h_ays, cache, id_range, filt):
-    h_ys = (h_ys,) if isinstance(h_ys, str) else tuple(
-        h_ys) if isinstance(h_ys, list) else h_ys
-    h_ays = (h_ays,) if isinstance(h_ays, str) else tuple(
-        h_ays) if isinstance(h_ays, list) else h_ays
-
-    y_c = [cache.get(hy) for hy in h_ys]
-
-    if not (all(y_c)):
-        raise PreventUpdate
-
-    if filt[0]:
-        y_c = [filter(y, filt[1]) for y in y_c]
-    # x = x_c[id_range[0]:id_range[1]]
-    ys = [y[id_range[0]:id_range[1]] for y in y_c]
-
-    if h_ays is not None and len(h_ays) > 0:
-        alt_y_c = [cache.get(hy) for hy in h_ays]
-
-        if filt[0]:
-            alt_y_c = [filter(y, filt[1]) for y in alt_y_c]
-        alt_ys = [y[id_range[0]:id_range[1]] for y in alt_y_c]
-
-    if len(y_c) > 0:
-        x_c = cache.get(h_x) if h_x is not None else [
-            k for k in range(len(y_c[0]))]
-        x = x_c[id_range[0]:id_range[1]]
-    elif len(alt_y_c) > 0:
-        x_c = cache.get(h_x) if h_x is not None else [
-            k for k in range(len(alt_y_c[0]))]
-        x = x_c[id_range[0]:id_range[1]]
-
-    second = None
-    if h_ays is not None and len(h_ays) > 0:
-        traces = [go.Scatter(x=x, y=yk, name=hyk)
-                  for yk, hyk in zip(ys+alt_ys, h_ys+h_ays)]
-        second = tuple(False for k in range(len(ys))) + \
-            tuple(True for k in range(len(alt_ys)))
-    else:
-        traces = [go.Scatter(x=x, y=yk, name=hyk) for yk, hyk in zip(ys, h_ys)]
-
-    return traces, second
 
 
 def filter(series, window):
@@ -149,20 +100,20 @@ def filter(series, window):
         nan_as_zeros, window, mode='constant', cval=0)
 
 
-def make_box(hx, hys, fns, cache):
+def make_box(col_x, cols_y, fns, data_dict):
 
     dfs = []
     for fn in fns:
         df = pd.DataFrame()
-        data = cache.get(fn)
+        data = data_dict.get(fn)
 
-        df[hx] = data.get(hx)
-        for hy in hys:
-            df[hy] = data.get(hy)
+        df[col_x] = data.get(col_x)
+        for col_y in cols_y:
+            df[col_y] = data.get(col_y)
 
         df['fn'] = fn
         dfs.append(df)
     plot_df = pd.concat(dfs)
 
-    fig = px.box(plot_df, x=hx, y=list(hys), color='fn')
+    fig = px.box(plot_df, x=col_x, y=list(cols_y), color='fn')
     return fig
